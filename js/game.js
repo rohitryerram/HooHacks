be293090f8dbf68d232d6b4d5246abd461eb4629
Jacob Benedetti <jacob.benedetti@uconn.edu>
2021-03-27 16:39:56 -0400

    basics

---
+++ js/game.js
@@ -0,0 +1,13 @@
+let gameScene = new Phaser.Scene('Game');
+
+gameScene.preload = function(){
+}
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360,
+    scene: gameScene
+}
+
+let game = new Phaser.Game(config);
