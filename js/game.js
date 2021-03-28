41d5b9aa399204345ea9d885227178c43123cabf
Jacob Benedetti <jacob.benedetti@uconn.edu>
2021-03-28 00:51:01 -0400

    student can move

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+var student;
+
 function preload () {
     this.load.image('bg', 'assets/classroom.png');
     this.load.image('student', 'assets/character.png');
@@ -6,11 +8,24 @@ function preload () {
 function create () {
     let background = this.add.image(0, 0, 'bg');
     background.setOrigin(0, 0);
-    let student = this.add.image(500, 750, 'student');
+    student = this.add.sprite(500, 750, 'student');
     student.setScale(0.2, 0.2);
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update () {
+    if (cursors.left.isDown) {
+        student.x -= 4;
+    }
+    else if (cursors.right.isDown) {
+        student.x += 4;
+    }
+    else if (cursors.up.isDown) {
+        student.y -= 4;
+    }
+    else if (cursors.down.isDown) {
+        student.y += 4;
+    }
 }
 
 let config = {
@@ -21,13 +36,6 @@ let config = {
         preload: preload,
         create: create,
         update: update
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300},
-            debug: false
-        }
     }
 }
 
