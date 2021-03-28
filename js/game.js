ddb87539cfc5027c6654e04e021f177c7be763c0
Jacob Benedetti <jacob.benedetti@uconn.edu>
2021-03-27 23:12:04 -0400

    updating UI

--- js/game.js
+++ js/game.js
@@ -1,13 +1,34 @@
-let gameScene = new Phaser.Scene('Game');
+function preload () {
+    this.load.image('bg', 'assets/classroom.png');
+    this.load.image('student', 'assets/character.png');
+}
 
-gameScene.preload = function(){
+function create () {
+    let background = this.add.image(0, 0, 'bg');
+    background.setOrigin(0, 0);
+    let student = this.add.image(500, 750, 'student');
+    student.setScale(0.2, 0.2);
+}
+
+function update () {
 }
 
 let config = {
     type: Phaser.AUTO,
-    width: 640,
-    height: 360,
-    scene: gameScene
+    width: 1648,
+    height: 900,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300},
+            debug: false
+        }
+    }
 }
 
 let game = new Phaser.Game(config);
